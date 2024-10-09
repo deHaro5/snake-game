1f4c6e21508893106df783a7de79a5ad3810e3cc
deHaro5 <frandeharo55@gmail.com>
2024-10-09 15:36:00 +0200

    Configurar frontend con index.html y scripts de build

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Snake Game 1v1</h1>
+      {/* Aquí iría el código de tu juego */}
+    </div>
+  );
+}
+
+export default App;
